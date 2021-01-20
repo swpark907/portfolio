d18c161b19c566bbe3982084bd4db95d2ca9a873
swpark907 <76856321+swpark907@users.noreply.github.com>
2021-01-20 17:25:09 +0900

    JS Navbar menu click

--- main.js
+++ main.js
@@ -6,13 +6,26 @@ const navBar = document.querySelector('#navbar')
 const navBarHeight = navBar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navBarHeight = ${navBarHeight}`);
-
     if(navBarHeight < window.scrollY) {
         navBar.classList.add('navbar__dark')
     } else{
         navBar.classList.remove('navbar__dark')
     }
     
+})
+
+// Screen move to section when user click Button
+
+const navbarMenu = document.querySelector('#navbar__list');
+navbarMenu.addEventListener('click', (event) => {
+    
+    const target = event.target;
+    const link = target.dataset.link;
+
+    if(link == null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
 })
