5f0ca7fb7f6061b5ce05e99c50929db94091fa62
swpark907 <76856321+swpark907@users.noreply.github.com>
2021-01-25 10:19:08 +0900

    HTML CSS delete footer

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ const navBar = document.querySelector('#navbar')
 const navBarHeight = navBar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    if(navBarHeight < window.scrollY) {
+    if (navBarHeight < window.scrollY) {
         navBar.classList.add('navbar__dark')
-    } else{
+    } else {
         navBar.classList.remove('navbar__dark')
     }
-    
+
 });
 
 
@@ -19,16 +19,18 @@ document.addEventListener('scroll', () => {
 
 const navbarMenu = document.querySelector('#navbar');
 navbarMenu.addEventListener('click', (event) => {
-    
+
     const target = event.target;
     const link = target.dataset.link;
 
-    if(link == null) {
+    if (link == null) {
         return;
     }
 
     const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollTo.scrollIntoView({
+        behavior: 'smooth'
+    });
     navbarList.classList.remove('open');
 
 });
@@ -48,9 +50,9 @@ document.addEventListener('scroll', () => {
 const arrow = document.querySelector('.arrow');
 
 document.addEventListener('scroll', () => { // if scroll under the home section, display the button
-    if(window.scrollY > homeHeight/3){
+    if (window.scrollY > homeHeight / 3) {
         arrow.classList.add('visible');
-    } else{
+    } else {
         arrow.classList.remove('visible');
     }
 });
@@ -61,7 +63,9 @@ arrow.addEventListener('click', () => { // if click the button, go to home secti
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollTo.scrollIntoView({
+        behavior: 'smooth'
+    });
 };
 
 // Filtering works
@@ -73,22 +77,21 @@ const projects = document.querySelectorAll('.project');
 categoriesContainer.addEventListener('click', (event) => {
     const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
 
-    if(filter == null) {
-        return;         
+    if (filter == null) {
+        return;
     }
     // Active
     const selected = document.querySelector('.category__btn.selected');
     selected.classList.remove('selected');
     const target = event.target.nodeName === 'BUTTON' ? event.target : event.target.parentNode;
-    target.classList.add('selected');    
-    
+    target.classList.add('selected');
+
     projectContainer.classList.add('anim-out');
     projects.forEach((project) => {
         setTimeout(() => {
             if (filter === '*' || project.dataset.type === filter) {
-                project.style.display = 'flex';                           
-            }
-            else{
+                project.style.display = 'flex';
+            } else {
                 project.style.display = 'none';
             }
             projectContainer.classList.remove('anim-out');
@@ -103,3 +106,38 @@ const navbarList = document.querySelector('#navbar__list');
 navbarToggleBtn.addEventListener('click', () => {
     navbarList.classList.toggle('open');
 })
+
+// 1.모든 섹션 요소들과 메뉴 아이템들을 가지고 온다.
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+
+// const sectionIds = [
+//     '#home',
+//     '#about',
+//     '#skills',
+//     '#work',
+//     '#testimonials',
+//     '#contact',
+// ]
+
+// const sections = sectionIds.map(id => document.querySelector(id));
+// const navItems = sectionIds.map(id =>
+//     document.querySelector(`[data-link='${id}']`));
+
+// const observerOptions = {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 0.3,
+// }
+
+// const observerCallback = (entries, observer) => {
+//     entries.forEach(entry => {
+//         if (!entry.isintersecting) {
+//             const index = sectionIds.indexOf(`#${entry.target.id}`);
+//             console.log(index, entry.target.id);
+//         }
+//     });
+
+// }
+// const observer = new IntersectionObserver(observerCallback, observerOptions)
+// sections.forEach(section => observer.observe(section));
